d267ceb1c08eae9404f3d33df14bf286095b9e2e
roguestone <roguestoneinweb@gmail.com>
2026-02-01 17:45:40 +0300

    feat: implement queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -141,8 +141,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(async (chain, promise) => {
+    const result = await chain;
+    const value = await promise;
+    return result + value;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
