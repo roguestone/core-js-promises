dbe635558f0c42e1e98316918390faa1d91ceaa0
roguestone <roguestoneinweb@gmail.com>
2026-02-01 17:31:38 +0300

    feat: implement getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -115,8 +115,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const wrappedPromises = promises.map((promise) =>
+    promise.then((value) => value).catch(() => null)
+  );
+
+  return Promise.all(wrappedPromises);
 }
 
 /**
