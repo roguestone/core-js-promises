08e7c9337d4c07191e88666ce4840f3946e80e13
roguestone <roguestoneinweb@gmail.com>
2026-02-01 17:10:07 +0300

    feat: implement getPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -39,8 +39,13 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+async function getPromiseResult(source) {
+  try {
+    await source;
+    return 'success';
+  } catch {
+    return 'fail';
+  }
 }
 
 /**
